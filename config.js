1fb6eb75d1018daf7a5a1cd9ab534336e02949ea
somxay <81745380+somxay@users.noreply.github.com>
2026-07-30 01:05:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
   // ---- GitHub Repository ທີ່ໃຊ້ເກັບ data/products.json, data/settings.json, images/ ----
 GITHUB_OWNER: 'somxay',
 GITHUB_REPO: 'mac2u',
-GITHUB_BRANCH: 'main',
+GITHUB_BRANCH: 'master',
   // ---- Paths ພາຍໃນ Repository ----
  // Paths ພາຍໃນ Repository
 PRODUCTS_PATH: 'products.json',
